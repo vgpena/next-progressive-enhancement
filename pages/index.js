af516130f57ab54ccacca1d193a9ae1d4afd9a86
Violet Peña <violet@Violets-MacBook-Pro.local>
2021-05-10 14:02:06 -0700

    Accordion.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,69 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+// import Image from 'next/image'
+import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+
+const sections = [
+  {
+    title: "title1",
+    body: "Doggo ipsum borkdrive heckin good boys wrinkler borking doggo super chub smol borking doggo with a long snoot for pats big ol very good spot, borkf most angery pupper I have ever seen heckin shoober long woofer length boy. What a nice floof long woofer shibe very hand that feed shibe, sub woofer. What a nice floof you are doin me a concern adorable doggo ruff heckin good boys pupper, doggorino tungg heckin angery woofer. Smol you are doin me a concern yapper heckin good boys you are doing me a frighten, doge blop. Porgo clouds shooberino length boy lotsa pats, doing me a frighten pats. length boy lotsa pats yapper. Ruff aqua doggo heck, thicc. Big ol pupper shoob ur givin me a spook extremely cuuuuuute, long woofer stop it fren. Very jealous pupper heck very taste wow wow very biscit, long bois you are doing me the shock. Many pats heck long woofer heckin angery woofer pats, thicc very jealous pupper borking doggo.",
+  },
+  {
+    title: "title2",
+    body: "Clouds you are doing me a frighten many pats doggorino ruff puggorino mlem doge long bois heckin good boys, doing me a frighten doggo pupperino bork maximum borkdrive extremely cuuuuuute long woofer. Heck pupperino most angery pupper I have ever seen you are doin me a concern many pats, sub woofer smol sub woofer. Heck shibe very jealous pupper super chub, borking doggo. Floofs sub woofer woofer, wow very biscit.",
+  },
+];
+
+function StaticAccordionSection(props) {
+  const { title, body } = props;
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{body}</p>
+    </div>
+  );
+}
+
+function DynamicAccordionSection(props) {
+  const { title, body } = props;
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div>
+      <h2
+        style={{ color: "tomato", cursor: "pointer" }}
+        role="button"
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        {title}
+      </h2>
+      <p style={{ display: isExpanded ? "block" : "none" }}>{body}</p>
+    </div>
+  );
+}
+
+function Accordion(props) {
+  const { sections } = props;
+  const [hasJS, setHasJS] = useState(false);
+
+  useEffect(() => {
+    console.log("JS detected");
+    setHasJS(true);
+  }, []);
+
+  return sections.map((section) => {
+    return (
+      <div key={section.title}>
+        {hasJS
+          ? DynamicAccordionSection(section)
+          : StaticAccordionSection(section)}
+      </div>
+    );
+  });
+}
 
 export default function Home() {
   return (
@@ -15,55 +78,8 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Accordion sections={sections} />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
